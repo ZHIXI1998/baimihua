01419cf7131107c281f63d6791b86979e8b638e5
wangweikang <wangweikang@dhcc.com.cn>
2023-08-11 17:58:28 +0800

    项目初始化

---
+++ src/main/java/com/hamster/baimihua/BaimihuaApplication.java
@@ -0,0 +1,13 @@
+package com.hamster.baimihua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaimihuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaimihuaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamster/baimihua/BaimihuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamster.baimihua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaimihuaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
