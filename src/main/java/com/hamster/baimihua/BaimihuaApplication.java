9057eeb3f8c8fb1e1cb380afbea9c5cad455ab1d
wangweikang1 <wangweikang1@sunline.cn>
2024-04-02 16:45:54 +0800

    启动类修改

--- src/main/java/com/hamster/baimihua/BaimihuaApplication.java
+++ src/main/java/com/hamster/baimihua/BaimihuaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BaimihuaApplication {
 
     public static void main(String[] args) {
+        //我是启动类
         SpringApplication.run(BaimihuaApplication.class, args);
     }
 
