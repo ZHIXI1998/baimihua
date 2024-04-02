d883cd0c762d1863ed0c11d600447a9c6895b677
wangweikang1 <wangweikang1@sunline.cn>
2024-04-02 16:58:20 +0800

    11

--- src/main/java/com/hamster/baimihua/BaimihuaApplication.java
+++ src/main/java/com/hamster/baimihua/BaimihuaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BaimihuaApplication {
 
     public static void main(String[] args) {
+        //
         SpringApplication.run(BaimihuaApplication.class, args);
     }
 
