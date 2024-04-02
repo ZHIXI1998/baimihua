b5f6ce7eaa8cbd6102b8ff6aa5f741580176d3d2
wangweikang1 <wangweikang1@sunline.cn>
2024-04-02 17:32:59 +0800

    Revert "1"
    This reverts commit 17d2a325b191e0fcf862f077045465145a6bd678.
    
    # Conflicts:
    #	src/main/java/com/hamster/baimihua/BaimihuaApplication.java

--- src/main/java/com/hamster/baimihua/BaimihuaApplication.java
+++ src/main/java/com/hamster/baimihua/BaimihuaApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BaimihuaApplication {
 
     public static void main(String[] args) {
-//1
-        //2
         SpringApplication.run(BaimihuaApplication.class, args);
     }
 
