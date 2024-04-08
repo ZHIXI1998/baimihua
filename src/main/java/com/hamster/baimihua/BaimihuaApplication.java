54aee262f7defb4750f7e4d2131aa72239bd72fe
wangweikang1 <wangweikang1@sunline.cn>
2024-04-08 14:56:12 +0800

    冲突啦

--- src/main/java/com/hamster/baimihua/BaimihuaApplication.java
+++ src/main/java/com/hamster/baimihua/BaimihuaApplication.java
@@ -9,6 +9,7 @@ public class BaimihuaApplication {
 
     public static void main(String[] args) {
         //1
+        //我是启动�
         SpringApplication.run(BaimihuaApplication.class, args);
     }
 
