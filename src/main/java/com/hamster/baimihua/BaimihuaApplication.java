a3e788578b8c1778aed7c576381b5a8fc144b2d8
wangweikang <wangweikang1@sunline.cn>
2025-03-02 11:14:02 +0800

    20250302 修改BaimihuaApplication注释内容

--- src/main/java/com/hamster/baimihua/BaimihuaApplication.java
+++ src/main/java/com/hamster/baimihua/BaimihuaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BaimihuaApplication {
 
     public static void main(String[] args) {
-        //2
+        //20250302 修改
         SpringApplication.run(BaimihuaApplication.class, args);
     }
 
